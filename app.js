5e595bdb4b3932910293cb7ca2bfa9d3b704d705
Tanvir <tr30365@gmail.com>
2022-07-02 23:25:57 +0600

    Problem solving by JavaScript

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-// Feet to Mile 
+// Feet to Mile
 function feetToMile(n){
     let calculation = n * 0.000189394;
     return calculation;
 }
-let result = feetToMile(10000);
-console.log(result)
+let result1 = feetToMile(10000);
+console.log(result1)
+
+// Wood Calculator
+function woodCalculator(chair, table, bed){
+    let total = chair * 1 + table * 3 + bed * 5;
+    return total;
+}
+let result2 = woodCalculator(5, 2, 1);
+console.log(result2);
