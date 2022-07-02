6410a88dd1d120a12fc3f7f88a199cf85f23ab39
Tanvir <tr30365@gmail.com>
2022-07-02 23:51:50 +0600

    Problem solving by JavaScript

--- app.js
+++ app.js
@@ -13,3 +13,21 @@ function woodCalculator(chair, table, bed){
 }
 let result2 = woodCalculator(1, 1, 1);
 console.log(result2);
+
+// Brick Calculator
+function brickCalculator(n){
+    if(n <= 10){
+        let first10 = (15 * 1000) * n;
+        return first10;
+    } 
+    if(n <= 20){
+        let secound10 = (n - 10) * (12 * 1000) + (15 * 1000) * 10;
+        return secound10;
+    }
+    if(n <= 30){
+        let third10 = (n - 20) * (10 * 1000) + (12 * 1000) * 10 + (15 * 1000) * 10;
+        return third10;
+    }
+}
+let result3 = brickCalculator(25);
+console.log(result3);
